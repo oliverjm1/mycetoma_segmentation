d2d9a3ebaf55ff2d5c4434b581ad774856aac873
oliverjm1 <75314670+oliverjm1@users.noreply.github.com>
2024-09-01 18:22:47 +0100

    change to save models based on lowest segmentation loss, and add this to loss array instead

--- scripts/train_multitask.py
+++ scripts/train_multitask.py
@@ -24,6 +24,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Training UNet Multitask, saving metrics")
     parser.add_argument('--run_name', type=str, required=True, help="Name of run, which will govern output file names")
     parser.add_argument('--epochs', type=int, required=True, help="Number of training epochs")
+    parser.add_argument('--lr', type=float, required=True, help="Learning Rate")
     parser.add_argument('--use_corrected_dataset', action='store_true', help="Flag to use corrected dataset")
     parser.add_argument('--with_augmentation', action='store_true', help="Perform data augmentation on training data")
     parser.add_argument('--leave_out_bad_cases', action='store_true', help="Don't train on bad cases")
@@ -124,23 +125,23 @@ train_accuracies = []
 val_accuracies = []
 
 # TRAIN
-num_epochs = args.epochs
 threshold = 0.5
-best_val_class_loss = np.inf
+best_val_seg_loss = np.inf
 
 # Specify optimiser and criterion
 seg_criterion = bce_dice_loss
 class_criterion = nn.BCELoss()
-l_rate = 5e-5
+l_rate = args.lr
 optimizer = optim.Adam(model.parameters(), lr=l_rate)
 
 best_model_path = os.path.join(output_dir, args.run_name + '_best_model.pth')
 
-for epoch in range(num_epochs):
+for epoch in range(args.epochs):
 
     # train mode
     model.train()
     running_loss = 0.0
+    running_seg_loss = 0.0
     dice_coeff = 0.0
     total_accuracy = 0.0
     n = 0    # counter for num of batches
@@ -165,12 +166,14 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         running_loss += total_loss.detach().cpu().numpy()
+        running_seg_loss += seg_loss.detach().cpu().numpy()
         dice_coeff += batch_dice_coeff(seg_outputs>threshold, targets).detach().cpu().numpy()
         total_accuracy += accuracy(class_outputs.squeeze().detach().cpu(), labels.detach().cpu())
         n += 1
 
     # Get train metrics, averaged over number of images in batch
     train_loss = running_loss/n
+    train_seg_loss = running_seg_loss/n
     train_dice_av = dice_coeff/n
     train_accuracy = total_accuracy/n
 
@@ -178,7 +181,7 @@ for epoch in range(num_epochs):
     # set model to eval mode and reset metrics
     model.eval()
     running_loss = 0.0
-    running_class_loss = 0.0
+    running_seg_loss = 0.0
     dice_coeff = 0.0
     total_accuracy = 0.0
     n = 0
@@ -200,33 +203,33 @@ for epoch in range(num_epochs):
             total_loss = seg_loss + (args.class_loss_weight * class_loss)
 
             running_loss += total_loss.detach().cpu().numpy()
-            running_class_loss += class_loss.detach().cpu().numpy()
+            running_seg_loss += seg_loss.detach().cpu().numpy()
             dice_coeff += batch_dice_coeff(seg_outputs>threshold, targets).detach().cpu().numpy()
             total_accuracy += accuracy(class_outputs.squeeze().detach().cpu(), labels.detach().cpu())
             n += 1
 
     # Val metrics
     val_loss = running_loss/n
-    val_class_loss = running_class_loss/n
+    val_seg_loss = running_seg_loss/n
     val_dice_av = dice_coeff/n
     val_accuracy = total_accuracy/n
 
     # print stats
     print(f"--------- EPOCH {epoch} ---------")
-    print(f"Train Loss: {train_loss}, Train Dice Score: {train_dice_av}, Train Accuracy: {train_accuracy * 100:.2f}%")
-    print(f"Val Loss: {val_loss}, Val Dice Score: {val_dice_av}, Val Accuracy: {val_accuracy * 100:.2f}%")
+    print(f"Train Loss: {train_loss} (Seg Loss: {train_seg_loss}), Train Dice Score: {train_dice_av}, Train Accuracy: {train_accuracy * 100:.2f}%")
+    print(f"Val Loss: {val_loss} (Seg Loss: {val_seg_loss}), Val Dice Score: {val_dice_av}, Val Accuracy: {val_accuracy * 100:.2f}%")
 
-    # save stats
-    train_losses.append(train_loss)
-    val_losses.append(val_loss)
+    # save stats (save segmentation loss only)
+    train_losses.append(train_seg_loss)
+    val_losses.append(val_seg_loss)
     train_dice_scores.append(train_dice_av)
     val_dice_scores.append(val_dice_av)
     train_accuracies.append(train_accuracy)
     val_accuracies.append(val_accuracy)
 
-    # Update best model if lowest loss
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
+    # Update best model if lowest loss (BASED ON SEGMENTATION LOSS)
+    if val_seg_loss < best_val_seg_loss:
+        best_val_seg_loss = val_seg_loss
         # Save the model
         torch.save(model.state_dict(), best_model_path)
         print('New best model saved')
