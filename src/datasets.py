91efff67ac2aeae8486777bbd155d8bf9b30d8eb
oliverjm1 <75314670+oliverjm1@users.noreply.github.com>
2024-09-01 17:42:14 +0100

    random word in there

--- src/datasets.py
+++ src/datasets.py
@@ -33,8 +33,6 @@ class MycetomaDataset(Dataset):
             image, mask = return_image_and_mask(self.data_dir, path, as_numpy=False)
             image, mask = self.transform(image, mask)
 
-            image
-
             # Turn from PIL to numpy array
             image = np.asarray(image)
             mask = np.asarray(mask)
